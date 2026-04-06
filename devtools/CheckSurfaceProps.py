3fde6710c17d9fdc5b2c7130d74ad1a8597d9827
Rewonez <yasumiakane1@gmail.com>
2026-04-06 16:19:54 +0800

    Update CheckSurfaceProps.py
    Fixed regex bug in "materialRE" (stray backslash caused silent match failure — who could've guessed...), replaced deprecated "os.popen" with "subprocess.check_output" (smh), added error handling (why no handling the error??), replaced manual "dict-as-set" pattern with proper "set()", and general cleanup.

--- devtools/CheckSurfaceProps.py
+++ devtools/CheckSurfaceProps.py
@@ -1,12 +1,12 @@
-
+# Refactored so it's clean I guess...
+# Valve Pls Fix TwT
 import WildcardSearch
 import sys
 import re
 import os
-
+import subprocess
 
 curProps = {}
-
 
 modelsContentFallbackDir = "c:\\hl2\\hl2\\models"
 modelsContentDir = "c:\\hl2\\cstrike\\models"
@@ -16,79 +16,62 @@ materialsDir = "c:\\hl2\\cstrike\\materials"
 mapsDir = "c:\\hl2\\cstrike\\maps"
 exeDir = "c:\\hl2\\bin"
 
-
 # RE to look for '$surfaceProp blah'
-surfacePropRE = re.compile( r'\"?\$surfaceprop\"?\s+\"?(?P<propname>[^\"]+)\"?', re.IGNORECASE )
-
+surfacePropRE = re.compile(r'\"?\$surfaceprop\"?\s+\"?(?P<propname>[^\"]+)\"?', re.IGNORECASE)
 
 
 # ------------------------------------------------------------------------------------------- #
 # Helper functions.
 # ------------------------------------------------------------------------------------------- #
 
-def FileExists(f):
-	try:
-		file = open(f)
-	except IOError:
-		exists = 0
-	else:
-		exists = 1
-		file.close()
-	return exists
+def FileExists(path):
+    return os.path.exists(path)
+# FileExists? Nah, os.path.exists.
+
+def SearchFile(filename):
+    with open(filename, "rt") as f:
+        PrintFilename(filename)
+        fileData = f.read()
+        match = surfacePropRE.search(fileData)
+        if match:
+            propName = match.group(1).upper()
+            curProps[propName] = 1
 
 
-def PrintFilename( filename ):
-	print filename
-
-
-def SearchFile( filename ):
-		f = open( filename, "rt" )
-			
-		PrintFilename( filename )
-
-		fileData = f.read()
-		match = surfacePropRE.search( fileData )
-		if match:
-			propName = match.group( 1 ).upper()
-			curProps[propName] = 1
-
-		f.close()
-
+def PrintFilename(filename):
+    print filename
 
 
 # ------------------------------------------------------------------------------------------- #
 # Search all the map files for texture names and model files.
 # ------------------------------------------------------------------------------------------- #
+# Set, changed this to set. Don't know if it works though, this used to be empty.
+usedVMTFiles = set()
+modelFiles = set()
 
-usedVMTFiles = {}
-modelFiles = {}
-
-
-# RE to look for 'material blah'
-materialRE = re.compile( r'\"?\material\"?\s+\"?(?P<matname>[^\"]+)\"?', re.IGNORECASE )
+# RE to look for 'material blah'  (fixed: removed stray backslash before 'material')
+materialRE = re.compile(r'\"?material\"?\s+\"?(?P<matname>[^\"]+)\"?', re.IGNORECASE)
 
 # Look for a model name referenced in the VMF file.
-modelRE = re.compile( r'\"models\/(?P<modelname>.+)\.mdl\"', re.IGNORECASE )
+modelRE = re.compile(r'\"models\/(?P<modelname>.+)\.mdl\"', re.IGNORECASE)
 
-
-files = WildcardSearch.WildcardSearch( mapsDir + "\\*.vmf", 1 )
+mapsPattern = os.path.join(mapsDir, "*.vmf")
+files = WildcardSearch.WildcardSearch(mapsPattern, 1)
 for filename in files:
-	f = open( filename, "rt" )
-	fileData = f.read()
-	f.close()
+    with open(filename, "rt") as f:
+        fileData = f.read()
 
-	PrintFilename( filename )
+    PrintFilename(filename)
 
-	# Get all the model names.
-	allMatches = modelRE.findall( fileData )
-	for match in allMatches:
-		modelFiles[match.upper()] = 1
+    # Get all the model names.
+    allMatches = modelRE.findall(fileData)
+    for match in allMatches:
+        modelFiles.add(match.upper())
 
-	# Get all the texture names.
-	allMatches = materialRE.findall( fileData )
-	for match in allMatches:
-		vmtName = match
-		usedVMTFiles[vmtName] = 1
+    # Get all the texture names.
+    allMatches = materialRE.findall(fileData)
+    for match in allMatches:
+        usedVMTFiles.add(match)
 
 
 # ------------------------------------------------------------------------------------------- #
@@ -96,44 +79,44 @@ for filename in files:
 # ------------------------------------------------------------------------------------------- #
 
 # Make sure we look at ALL models in the CStrike folder.
-for filename in WildcardSearch.WildcardSearch( modelsContentDir + "\\*.mdl", 1 ):
-	modelFiles[filename.upper()] = 1
+modelsPattern = os.path.join(modelsContentDir, "*.mdl")
+for filename in WildcardSearch.WildcardSearch(modelsPattern, 1):
+    modelFiles.add(filename.upper())
 
-for iModel in modelFiles.keys():
-	iModel = iModel.replace( "/", "\\" )
-	filename = modelsContentDir + "\\" + iModel + ".mdl"
-	if not FileExists( filename ):
-		filename = modelsContentFallbackDir + "\\" + iModel + ".mdl"
+for iModel in modelFiles:
+    iModel = iModel.replace("/", "\\")
+    filename = os.path.join(modelsContentDir, iModel + ".mdl")
+    if not FileExists(filename):
+        filename = os.path.join(modelsContentFallbackDir, iModel + ".mdl")
+# here lies what used to be deprecated code
+    if FileExists(filename):
+        PrintFilename(filename)
 
-	if FileExists( filename ):
-		PrintFilename( filename )
+        # I changed something here, it might break.
+        cmd = '"%s\\studiomdl.exe" -PrintSurfaceProps "%s"' % (exeDir, filename)
+        try:
+            output = subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT)
+            for line in output.splitlines():
+                curProps[line.upper().strip()] = 1
+        except subprocess.CalledProcessError as e:
+            print >>sys.stderr, "Error running studiomdl on %s: %s" % (filename, e)
+        except OSError as e:
+            print >>sys.stderr, "studiomdl.exe not found: %s" % e
 
-		cmd = exeDir + "\\studiomdl.exe -PrintSurfaceProps " + filename
-		f = os.popen( cmd )
-		if f:
-			output = f.readlines()
-			returnValue = f.close()
-
-			if returnValue == None:
-				for line in output:
-					curProps[line.upper().strip()] = 1
-
-
+# Tried error handling here for missing studiomdl.exe
 
 # ------------------------------------------------------------------------------------------- #
 # Search all the texture files for surface props.
 # ------------------------------------------------------------------------------------------- #
 
-for iFile in usedVMTFiles.keys():
-	filename = materialsDir + "\\" + iFile + ".vmt"
-	if not FileExists( filename ):
-		filename = materialsFallbackDir + "\\" + iFile + ".vmt"
-	
-	if FileExists( filename ):
-		SearchFile( filename )
+for vmtName in usedVMTFiles:
+    filename = os.path.join(materialsDir, vmtName + ".vmt")
+    if not FileExists(filename):
+        filename = os.path.join(materialsFallbackDir, vmtName + ".vmt")
 
+    if FileExists(filename):
+        SearchFile(filename)
 
-			
 
 # ------------------------------------------------------------------------------------------- #
 # Output the results.
@@ -144,10 +127,6 @@ print "---------------------------------"
 print "- Surface types found"
 print "---------------------------------\n"
 
-sortedList = [x for x in curProps.keys()]
-sortedList.sort()
-
+sortedList = sorted(curProps.keys())
 for x in sortedList:
-	print x
-
-
+    print x
