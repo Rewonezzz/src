79c2b87301e3845ba8d786d353893080d94552f9
Rewonez <yasumiakane1@gmail.com>
2026-04-07 00:00:09 +0800

    Update AddFunctionPrologue.py (Valve pls fix)
    Cleaned up AddFunctionPrologue.py — removed dead code and added several changes and fixes.
    
    Removed unused "BlankStruct" class (so many dead code). 
    Renamed "list" to "tokens" and "nw" to "non_whitespace" to avoid shadowing built‑ins.
    Fixed regex escapes for " " and "" using raw strings (similar to FindClasses.py lol). 
    Replaced "while 1" with "while True". Simplified "validChars" regex escaping. 
    Replaced manual "open()" / "close()" with "with" blocks. Added bounds check "pos - 1 >= 0" to prevent "IndexError". 
    Removed an unnecessary second read of the file (redundant "readlines" block — why?).

--- devtools/AddFunctionPrologue.py
+++ devtools/AddFunctionPrologue.py
@@ -1,5 +1,4 @@
-
-
+# Dead code everywhere, what the hell...
 # Assuming all functions begin with ')' followed by '{', just find the matching brace and
 # add a line with 'g_pVCR->SyncToken("<random string here>");'
 
@@ -7,110 +6,94 @@ import dlexer
 import sys
 
 
-class BlankStruct:
-	pass
+def MatchParensBack(token_list, iStart):
+    parenCount = -1
+    for i in range(0, iStart):
+        if token_list[iStart - i].id == __TOKEN_OPENPAREN:
+            parenCount += 1
+        elif token_list[iStart - i].id == __TOKEN_CLOSEPAREN:
+            parenCount -= 1
+
+        if parenCount == 0:
+            return iStart - i
+    return -1
 
 
-def MatchParensBack( list, iStart ):
-	parenCount = -1
-	for i in range( 0, iStart ):
-		if list[iStart-i].id == __TOKEN_OPENPAREN:
-			parenCount += 1
-		elif list[iStart-i].id == __TOKEN_CLOSEPAREN:
-			parenCount -= 1
-		
-		if parenCount == 0:
-			return iStart - i
-	
-	return -1
+if len(sys.argv) >= 2:
 
+    # Setup the parser.
+    parser = dlexer.DLexer(0)
+# Regex cleaned
+    __TOKEN_NEWLINE = parser.AddToken('\n')
+    __TOKEN_WHITESPACE = parser.AddToken('[ \\t\\f\\v]+')
+    __TOKEN_OPENBRACE = parser.AddToken('{')
+    __TOKEN_CLOSEBRACE = parser.AddToken('}')
+    __TOKEN_OPENPAREN = parser.AddToken(r'\(')
+    __TOKEN_CLOSEPAREN = parser.AddToken(r'\)')
+    __TOKEN_COMMENT = parser.AddToken(r"\/\/.*")
 
-if len( sys.argv ) >= 2:
+    __TOKEN_CONST = parser.AddToken("const")
+    __TOKEN_IF = parser.AddToken("if")
+    __TOKEN_WHILE = parser.AddToken("while")
+    __TOKEN_FOR = parser.AddToken("for")
+    __TOKEN_SWITCH = parser.AddToken("switch")
 
-	# Setup the parser.
-	parser = dlexer.DLexer( 0 )
+    validChars = r"~@#$%^&!,\w\.\-/\[\]<>\""
+    __TOKEN_IDENT = parser.AddToken('[' + validChars + ']+')
+    __TOKEN_OPERATOR = parser.AddToken(r"=|\+")
+    __TOKEN_SCOPE_OPERATOR = parser.AddToken("::")
+    __TOKEN_IGNORE = parser.AddToken(r"#|;|:|\||\?|'|\\|\*|-|`")
 
-	__TOKEN_NEWLINE = parser.AddToken( '\n' )
-	__TOKEN_WHITESPACE = parser.AddToken( '[ \\t\\f\\v]+' )
-	__TOKEN_OPENBRACE  = parser.AddToken( '{' )
-	__TOKEN_CLOSEBRACE = parser.AddToken( '}' )
-	__TOKEN_OPENPAREN  = parser.AddToken( '\(' )
-	__TOKEN_CLOSEPAREN = parser.AddToken( '\)' )
-	__TOKEN_COMMENT = parser.AddToken( r"\/\/.*" )
-	
-	__TOKEN_CONST = parser.AddToken( "const" )
-	__TOKEN_IF = parser.AddToken( "if" )
-	__TOKEN_WHILE = parser.AddToken( "while" )
-	__TOKEN_FOR = parser.AddToken( "for" )
-	__TOKEN_SWITCH = parser.AddToken( "switch" )
-	
-	validChars = r"\~\@\#\$\%\^\&\!\,\w\.-/\[\]\<\>\""
-	__TOKEN_IDENT = parser.AddToken( '[' + validChars + ']+' )
-	__TOKEN_OPERATOR = parser.AddToken( "\=|\+" )
-	__TOKEN_SCOPE_OPERATOR = parser.AddToken( "::" )
-	__TOKEN_IGNORE = parser.AddToken( r"\#|\;|\:|\||\?|\'|\\|\*|\-|\`" )
+    # First, read all the tokens into a list.
+    tokens = []
+    parser.BeginReadFile(sys.argv[1])
+    while True:
+        m = parser.GetToken()
+        if m:
+            tokens.append(m)
+        else:
+            break
 
-	head = None
+    # Make a list of all the non-whitespace ones.
+    non_whitespace = []
+    for token in tokens:
+        if token.id in (__TOKEN_NEWLINE, __TOKEN_WHITESPACE):
+            token.iNonWhitespace = -2222
+        else:
+            token.iNonWhitespace = len(non_whitespace)
+            non_whitespace.append(token)
 
-	# First, read all the tokens into a list.
-	list = []
-	parser.BeginReadFile( sys.argv[1] )
-	while 1:
-		m = parser.GetToken()
-		if m:
-			list.append( m )
-		else:
-			break
+    # Write directly to the original file (but with 'with' to ensure closure! Makes it automatic)
+    with open(sys.argv[1], 'w') as out_file:
+        curLine = 1
+        iCur = 0
 
-	
-	# Make a list of all the non-whitespace ones.
-	nw = []
-	for token in list:
-		if token.id == __TOKEN_NEWLINE or token.id == __TOKEN_WHITESPACE:
-			token.iNonWhitespace = -2222
-		else:
-			token.iNonWhitespace = len( nw )
-			nw.append( token )
+        # Now, search for the patterns
+        # Look for <ident>::<ident> '(' <idents...> ')' followed by a '{'. This would be a function.
+        for token in tokens:
+            out_file.write(token.val)
+            if token.id == __TOKEN_NEWLINE:
+                curLine += 1
 
-	
-	# Get ready to output sync tokens.
-	file = open( sys.argv[1], 'r' )
-	fileLines = file.readlines()
-	file.close()
+            if token.id == __TOKEN_OPENBRACE:
+                idx = token.iNonWhitespace
+                # Bounds checking (needed): need at least 6 non-whitespace tokens before this brace,
+                # and the previous token must be a closing parenthesis.
+                if idx >= 6 and non_whitespace[idx - 1].id == __TOKEN_CLOSEPAREN:
+                    pos = MatchParensBack(non_whitespace, idx - 2)
+                    # Ensure pos is valid and there's an identifier before it.
+                    if pos != -1 and pos - 1 >= 0:
+                        if non_whitespace[pos - 1].id == __TOKEN_IDENT:
+                            # ADD PROLOGUE CODE HERE!!
+                            # out_file.write("\n\tg_pVCR->SyncToken( \"%d_%s\" ); // AUTO-GENERATED SYNC TOKEN\n" % (iCur, non_whitespace[pos - 1].val))
+                            iCur += 1
 
-	
-	curLine = 1
-	iCur = 0
-
-	file = open( sys.argv[1], 'w' )
-
-	# Now, search for the patterns we're interested in.
-	# Look for <ident>::<ident> '(' <idents...> ')' followed by a '{'. This would be a function.
-	for token in list:
-		file.write( token.val )
-		if token.id == __TOKEN_NEWLINE:
-			curLine += 1
-
-		if token.id == __TOKEN_OPENBRACE:
-			i = token.iNonWhitespace
-			if i >= 6:
-				if nw[i-1].id == __TOKEN_CLOSEPAREN:
-					pos = MatchParensBack( nw, i-2 )
-					if pos != -1:
-						if nw[pos-1].id == __TOKEN_IDENT:
-							#ADD PROLOGUE CODE HERE
-							#file.write( "\n\tg_pVCR->SyncToken( \"%d_%s\" ); // AUTO-GENERATED SYNC TOKEN\n" % (iCur, nw[pos-1].val) )
-							iCur += 1
-							
-							# TEST CODE TO PRINT OUT FUNCTION NAMES
-							#if nw[pos-2].id == __TOKEN_SCOPE_OPERATOR:
-							#	print "%d: %s::%s" % ( curLine, nw[pos-3].val, nw[pos-1].val )
-							#else:
-							#	print "%d: %s" % ( curLine, nw[pos-1].val )
-
-	file.close()
+                            # Test code to print function names or something.
+                            # if pos - 2 >= 0 and non_whitespace[pos - 2].id == __TOKEN_SCOPE_OPERATOR:
+                            #     print "%d: %s::%s" % (curLine, non_whitespace[pos - 3].val, non_whitespace[pos - 1].val)
+                            # else:
+                            #     print "%d: %s" % (curLine, non_whitespace[pos - 1].val)
 
 else:
-
-	print "VCRMode_AddSyncTokens <filename>"
-
+    print "VCRMode_AddSyncTokens <filename>"
+# Python 2!
