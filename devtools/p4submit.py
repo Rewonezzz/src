929076c7f905c5f1364c35beb41d7337bbaede7b
Rewonez <yasumiakane1@gmail.com>
2026-04-20 22:21:49 +0800

    Update p4submit.py (fix pls Valve)
    THERE'S NO ERROR HANDLING??? Added that, and uh — I don't know... Removed the unused "import pdb" — and made it compatible with both Python 2/3 — hopefully.

--- devtools/p4submit.py
+++ devtools/p4submit.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+# Okay, this has to stop, I'm tired boss...
+from __future__ import print_function
 
 import getopt
 import os
@@ -6,116 +8,134 @@ import re
 import sys
 import subprocess
 
-import pdb
-
 def usage():
-	print >> sys.stderr, 'checks in all open files in the default changelist, aggregating change comments from the provided list'
-	print >> sys.stderr, ''
-	print >> sys.stderr, 'usage:'
-	print >> sys.stderr, sys.argv[0] + ' [-c p4client] [-p p4port] [-u p4user] [-d changelog (prefix)] [--changes "list of change numbers"]'
-	print >> sys.stderr, ''
-	print >> sys.stderr, 'the list of changes must either be the last argument, or be quoted, so '
-	print >> sys.stderr, sys.argv[0] + ' ... --changes 1 2 3  [ok] '
-	print >> sys.stderr, sys.argv[0] + ' ... --changes "1 2 3" ... [ok]'
-	print >> sys.stderr, sys.argv[0] + ' ... -changes 1 2 3 ... [bad]'
-	
+    print('checks in all open files in the default changelist, aggregating change comments from the provided list', file=sys.stderr)
+    print('', file=sys.stderr)
+    print('usage:', file=sys.stderr)
+    print(sys.argv[0] + ' [-c p4client] [-p p4port] [-u p4user] [-d changelog (prefix)] [--changes "list of change numbers"]', file=sys.stderr)
+    print('', file=sys.stderr)
+    print('the list of changes must either be the last argument, or be quoted, so ', file=sys.stderr)
+    print(sys.argv[0] + ' ... --changes 1 2 3  [ok] ', file=sys.stderr)
+    print(sys.argv[0] + ' ... --changes "1 2 3" ... [ok]', file=sys.stderr)
+    print(sys.argv[0] + ' ... -changes 1 2 3 ... [bad]', file=sys.stderr)
+
 def main():
-	try:
-		opts, args = getopt.getopt( sys.argv[1:], "c:p:u:d:", [ "changes=" ] )
-	except getopt.GetoptError, err:
-		print >> sys.stderr, str(err)
-		usage()
-		sys.exit(-1)
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "c:p:u:d:", ["changes="])
+    except getopt.GetoptError as err:
+        print(str(err), file=sys.stderr)
+        usage()
+        sys.exit(-1)
 
-	p4user = None
-	p4client = None
-	p4port = None
-	changelog = None
-	changes = []
-	p4cmdbase = [ "p4" ]
-	for opt, arg in opts:
-		if opt == "-c":
-			p4cmdbase.extend( [ opt, arg ] )
-			p4client = arg
-		elif opt == "-p":
-			p4cmdbase.extend( [ opt, arg ] )
-			p4port = arg
-		elif opt == "-u":
-			p4cmdbase.extend( [ opt, arg ] )
-			p4user = arg
-		elif opt == "-d":
-			# eat this one, we'll build our own changespec
-			changelog = arg
-		elif opt == "--changes":
-			# and these are the change #'s to include descriptions of
-			changes = arg.split()
-			if args is not None:
-				changes.extend( args )
+    p4user = None
+    p4client = None
+    p4port = None
+    changelog = None
+    changes = []
+    p4cmdbase = ["p4"]
+    for opt, arg in opts:
+        if opt == "-c":
+            p4cmdbase.extend([opt, arg])
+            p4client = arg
+        elif opt == "-p":
+            p4cmdbase.extend([opt, arg])
+            p4port = arg
+        elif opt == "-u":
+            p4cmdbase.extend([opt, arg])
+            p4user = arg
+        elif opt == "-d":
+            changelog = arg
+        elif opt == "--changes":
+            changes = arg.split()
+            if args:
+                changes.extend(args)
 
-	if p4user is None:
-		p4user = os.getenv( "P4USER" )
-	if p4client is None:
-		p4client = os.getenv( "P4CLIENT" )
-	if p4port is None:
-		p4port = os.getenv( "P4PORT" )
-		
-	if p4user is None or p4client is None or p4port is None:
-		print >> sys.stderr, "one or more p4 environment variables (p4user, p4client, p4port) aren't set." 
-		usage()
-		sys.exit(-1)
+    if p4user is None:
+        p4user = os.getenv("P4USER")
+    if p4client is None:
+        p4client = os.getenv("P4CLIENT")
+    if p4port is None:
+        p4port = os.getenv("P4PORT")
 
-	
-	# get the list of opened files 
-	openFiles = []
-	stdout = subprocess.Popen( " ".join( p4cmdbase ) + " opened -c default", shell=True, stdout=subprocess.PIPE ).stdout
-	lines = stdout.readlines()
-	stdout.close()
+    if p4user is None or p4client is None or p4port is None:
+        print("one or more p4 environment variables (p4user, p4client, p4port) aren't set.", file=sys.stderr)
+        usage()
+        sys.exit(-1)
 
-	filere = re.compile( "(.*)#.*\n" )
-	for line in lines:
-		m = filere.match( line )
-		if m is not None:
-			openFiles += [ m.groups()[ 0 ] ]
+    # Get the list of opened files.
+    try:
+        output = subprocess.check_output(
+            p4cmdbase + ["opened", "-c", "default"],
+            universal_newlines=True,
+            stderr=subprocess.PIPE
+        )
+    except subprocess.CalledProcessError as e:
+        print("Error running 'p4 opened': {}".format(e.stderr.strip()), file=sys.stderr)
+        sys.exit(e.returncode)
+    except FileNotFoundError:
+        print("Error: 'p4' command not found.", file=sys.stderr)
+        sys.exit(1)
 
-	if openFiles == []:
-		sys.stderr.write( "no files to submit from the default changelist" )
-		sys.exit(0)
-		
-	# get the changenotes from the specified changes
-	changeLines = []
-	for change in changes:
-		stdout = subprocess.Popen( " ".join( p4cmdbase ) + " describe -s " + change, shell=True, stdout=subprocess.PIPE ).stdout
-		lines = stdout.readlines()
-		stdout.close()
+    open_files = []
+    filere = re.compile(r"(.*)#.*\n")
+    for line in output.splitlines(True):
+        m = filere.match(line)
+        if m is not None:
+            open_files.append(m.groups()[0])
 
-		stopre = re.compile( "Affected files ..." )
-		for line in lines:
-			if stopre.match( line ):
-				break
-			changeLines += [ line.strip() ]
+    if not open_files:
+        sys.stderr.write("no files to submit from the default changelist\n")
+        sys.exit(0)
 
-	change_spec = ''
-	change_spec += 'Change: new\n'
-	change_spec += 'Client: %s\n' % p4client
-	change_spec += 'User: %s\n' % p4user
-	change_spec += 'Description:\n'
+    # Get the change notes from the specified changes.
+    change_lines = []
+    for change in changes:
+        try:
+            desc_output = subprocess.check_output(
+                p4cmdbase + ["describe", "-s", change],
+                universal_newlines=True,
+                stderr=subprocess.PIPE
+            )
+        except subprocess.CalledProcessError as e:
+            print("Error running 'p4 describe -s {}': {}".format(change, e.stderr.strip()), file=sys.stderr)
+            sys.exit(e.returncode)
 
-	# if they supplied a changelog line, prepend that to the list of changes
-	if changelog is not None:
-		change_spec += '\t%s\n\n' % changelog
-	# and now each line from all the changelists
-	if changeLines != []:
-		change_spec += '\t%s\n\n' % "Changes included in this submit:"
-	for changeLine in changeLines:
-		change_spec += '\t%s\n' % changeLine
+        stopre = re.compile(r"Affected files \.\.\.")
+        for line in desc_output.splitlines():
+            if stopre.match(line):
+                break
+            change_lines.append(line.strip())
 
-	change_spec += 'Files:\n'
-	for file in openFiles:
-		change_spec += '\t%s\n' % file
+    change_spec = ''
+    change_spec += 'Change: new\n'
+    change_spec += 'Client: {}\n'.format(p4client)
+    change_spec += 'User: {}\n'.format(p4user)
+    change_spec += 'Description:\n'
 
-	p = subprocess.Popen( " ".join( p4cmdbase ) + " submit -i", shell=True, stdin=subprocess.PIPE, stdout=sys.stdout, stderr=sys.stderr )
-	p.communicate( change_spec )
-	sys.exit( p.returncode )
+    if changelog is not None:
+        change_spec += '\t{}\n\n'.format(changelog)
+    if change_lines:
+        change_spec += '\t{}\n\n'.format("Changes included in this submit:")
+    for line in change_lines:
+        change_spec += '\t{}\n'.format(line)
+
+    change_spec += 'Files:\n'
+    for fname in open_files:
+        change_spec += '\t{}\n'.format(fname)
+
+    try:
+        proc = subprocess.Popen(
+            p4cmdbase + ["submit", "-i"],
+            stdin=subprocess.PIPE,
+            stdout=sys.stdout,
+            stderr=sys.stderr,
+            universal_newlines=True
+        )
+        proc.communicate(change_spec)
+        sys.exit(proc.returncode)
+    except Exception as e:
+        print("Error submitting changelist: {}".format(e), file=sys.stderr)
+        sys.exit(1)
 
 if __name__ == '__main__':
-	main()
+    main()
