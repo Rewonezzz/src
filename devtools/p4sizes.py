f468a27705995d86f5fb92e8afb5c80b2712f780
Rewonez <yasumiakane1@gmail.com>
2026-04-28 19:36:45 +0800

    Update p4sizes.py (Valve pls fix)
    Refactored the script to improve error handling (errno mentioned) and readability. Updated usage instructions and removed deprecated code.

--- devtools/p4sizes.py
+++ devtools/p4sizes.py
@@ -1,37 +1,81 @@
-
-import sys, re, os
-
-
-if len( sys.argv ) < 3:
-	print 'p4sizes.py shows the size of the last N revisions of a file in Perforce'
-	print 'usage: p4sizes.py filename N'
-	print 'ex   : p4size.py //valvegames/rel/hl2/game/bin/engine.dll 10'
-	print '       (shows the sizes of the last 10 checkins to engine.dll)'
-	sys.exit( 1 )
-
-filename = sys.argv[1]
-nRevisions = sys.argv[2]
+# Refactored for stuff...
+# p4debugscan changed to use errno module as well
+from __future__ import print_function
+import errno # for error handling
+import os
+import re
+import subprocess
+import sys
 
 
-# Get the revisions list.
-f = os.popen( 'p4 changes -m %s %s' % (nRevisions, filename) )
-str = f.read()
-f.close()
+def print_usage():
+    """Print usage information and exit."""
+    print('p4sizes.py shows the size of the last N revisions of a file in Perforce')
+    print('usage: p4sizes.py filename N')
+    print('ex   : p4sizes.py //valvegames/rel/hl2/game/bin/engine.dll 10')
+    print('       (shows the sizes of the last 10 checkins to engine.dll)')
+    sys.exit(1)
 
 
-changelistNumbers = []
-myRE = re.compile( r'change (?P<num>\d+?) on (?P<date>.+?) ', re.IGNORECASE )
-while 1:
-	m = myRE.search( str )
-	if m:
-		str = str[m.end():]
-		changelistNumbers.append( [m.group('num'), m.group('date')] )
-	else:
-		break
-		
-for x in changelistNumbers:
-	sys.stdout.write( x[1] + ': ' )
-	cmd = 'p4 sizes %s@%s' % (filename, x[1])
-	os.system( cmd )
+def get_changelist_numbers(filename, n_revisions):
+    cmd = ['p4', 'changes', '-m', str(n_revisions), filename]
+    try:
+        output = subprocess.check_output(
+            cmd, stderr=subprocess.STDOUT, universal_newlines=True
+        )
+    except subprocess.CalledProcessError as e:
+        print("Error running 'p4 changes': {}".format(e.output.strip()),
+              file=sys.stderr)
+        sys.exit(1)
+    except OSError as e:
+        if e.errno == errno.ENOENT:
+            print("Error: 'p4' command not found.", file=sys.stderr)
+        else:
+            print("Error: {}".format(e), file=sys.stderr)
+        sys.exit(1)
+
+    changelist_numbers = []
+    changelist_re = re.compile(
+        r'change (?P<num>\d+?) on (?P<date>.+?) ', re.IGNORECASE
+    )
+    remaining = output
+
+    while True:
+        match = changelist_re.search(remaining)
+        if not match:
+            break
+        remaining = remaining[match.end():]
+        changelist_numbers.append([match.group('num'), match.group('date')])
+
+    return changelist_numbers
 
 
+def get_file_size(filename, change_num):
+    cmd = ['p4', 'sizes', '{}@{}'.format(filename, change_num)]
+    try:
+        return subprocess.check_output(
+            cmd, stderr=subprocess.STDOUT, universal_newlines=True
+        )
+    except subprocess.CalledProcessError as e:
+        print("Error running 'p4 sizes' for changelist {}: {}".format(
+            change_num, e.output.strip()), file=sys.stderr)
+        return None
+
+
+def main():
+    if len(sys.argv) < 3:
+        print_usage()
+
+    filename = sys.argv[1]
+    n_revisions = sys.argv[2]
+
+    changelist_numbers = get_changelist_numbers(filename, n_revisions)
+
+    for change_num, change_date in changelist_numbers:
+        size_output = get_file_size(filename, change_num)
+        if size_output is not None:
+            print('{}: {}'.format(change_date, size_output.rstrip()))
+
+
+if __name__ == '__main__':
+    main()
