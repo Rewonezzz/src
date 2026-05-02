988e0ac8a9dbb7a89d738d728883ba361411e3b0
ThePixelMoon <yourlocalmoon7@gmail.com>
2026-05-02 17:44:19 +0300

    try to fix linux i3898585481745874 compiling

--- scripts/build-ubuntu-amd64.sh
+++ scripts/build-ubuntu-amd64.sh
@@ -2,7 +2,7 @@
 git pull
 git submodule init && git submodule update
 sudo apt-get update
-sudo apt-get install -f -y libopenal-dev g++-multilib gcc-multilib libpng-dev libjpeg-dev libfreetype6-dev libfontconfig1-dev libcurl4-gnutls-dev libsdl2-dev zlib1g-dev libbz2-dev libedit-dev automake autoconf libtool
+sudo apt-get install -f -y libopenal-dev g++-multilib gcc-multilib libpng-dev libjpeg-dev libfreetype6-dev libfontconfig1-dev libcurl4-gnutls-dev libsdl2-dev zlib1g-dev libbz2-dev libedit-dev automake autoconf libtool libssl-dev
 
 git clone --recursive --depth 1 https://github.com/xiph/opus
 cd opus
