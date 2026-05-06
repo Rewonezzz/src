d7cc10a66091bc8a4f12267020ecf2f177fcfa3e
ThePixelMoon <yourlocalmoon7@gmail.com>
2026-05-06 08:54:20 +0300

    game: shared: refactor lammodef

--- game/shared/lua/lammodef.h
+++ game/shared/lua/lammodef.h
@@ -1,16 +1,32 @@
+//========== Copyright (C) 2026, Team HL2SB++, All rights reserved. ===========//
+//
+// Purpose:
+//
+//===========================================================================//
+
 #ifndef LAMMODEF_H
 #define LAMMODEF_H
 #ifdef _WIN32
 #pragma once
 #endif
 
-#include "ammodef.h"
-#include "lua.hpp"
-
+/* type for CAmmoDef functions */
 typedef CAmmoDef lua_CAmmoDef;
 
+
+/*
+** access functions (stack -> C)
+*/
+
 LUA_API lua_CAmmoDef* lua_toammodef(lua_State* L, int idx);
+
 LUALIB_API lua_CAmmoDef* luaL_checkammodef(lua_State* L, int narg);
+
+
+/*
+** push functions (C -> stack)
+*/
+
 LUA_API void lua_pushammodef(lua_State* L, lua_CAmmoDef* pAmmoDef);
 
 #endif
